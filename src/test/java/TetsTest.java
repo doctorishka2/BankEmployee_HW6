49761f8ebd0298bb72a5eab7778268fcabf6e16c
g67oleg@gmail.com <kharkov67>
2024-05-30 18:45:08 -0400

    New TetstTest added

--- src/test/java/TetsTest.java
+++ src/test/java/TetsTest.java
@@ -3,6 +3,8 @@ public class TetsTest {
     public static void main(String[] args) {
 
         System.out.println("TestTest");
+        System.out.println("TestTest");
+        System.out.println("TestTest");
     }
 
 
